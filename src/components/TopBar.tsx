9b97fb135ea79aa0c73c27d54a4a888adc282243
samuelajayii <ajayisamuel023@gmail.com>
2023-11-02 23:31:46 +0100

    added the top nav functionality

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,17 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { faBell } from "@fortawesome/free-regular-svg-icons"
+import { faClockRotateLeft, faGears } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 const TopBar = () => {
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-let timeFunction: Function
 
-
+    const d = new Date()
+    let time: string = ""
+    if(d.getHours() < 12){
+        time = "Good Morning"
+    } else if (d.getHours() === 12){
+        time = "It's Noon!"
+    } else if (d.getHours() <= 17){
+        time = "Good Afternoon"
+    } else if (d.getHours() >= 18){
+        time = "Good Evening"
+    }
     
 
     return(
-        <header className="flex items-center justify-between bg-gradient-to-b py-3">
-            <h1 id="time" className="font-bold text-2xl md:text-5xl  py-2 px-2">Hello</h1>
+        <header className="flex items-center justify-between bg-gradient-to-b">
+            <h1 id="time" className="font-bold text-2xl md:text-5xl  py-2 px-2">{time}</h1>
             <div className="flex justify-betweeen items-center justify-between mt-2">
-                <i className="fa-regular fa-bell  cursor-pointer mr-4 lg:text-5xl lg:mr-16 md:mr-12 md:text-3xl"></i>
-                <i className="fa-solid fa-clock-rotate-left  mr-4 cursor-pointer lg:text-5xl lg:mr-16 md:mr-12 md:text-3xl"></i>
-                <i className="fa-solid fa-gears cursor-pointer  mr-4 lg:text-5xl md:mr-12 md:text-3xl"></i>
+                <FontAwesomeIcon icon={faBell} className="cursor-pointer mr-4 lg:text-5xl lg:mr-16 md:mr-12 md:text-3xl"/>
+                <FontAwesomeIcon icon={faClockRotateLeft} className="cursor-pointer mr-4 lg:text-5xl lg:mr-16 md:mr-12 md:text-3xl"/>
+                <FontAwesomeIcon icon={faGears} className="cursor-pointer mr-4 lg:text-5xl md:mr-12 md:text-3xl"/>
             </div>
         </header>
         
